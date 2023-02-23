47870b235b4806e38e171801f9c6d0b565dcbecb
zinakalu <zina.kalu@gmail.com>
2023-02-23 14:56:40 -0500

    tasklister project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+// const task = document.getElementById('create-task-form')
+// const submitbtn = document.getElementById('submitbtn')
+const main = document.getElementById("main-content")
+const input = document.getElementById('new-task-description')
+const submitbtn = document.getElementById('submitbtn')
+const list = document.getElementById('list')
+
+
+main.addEventListener("submit", (e) => {
   // your code here
+e.preventDefault()
+const main1 = document.createElement('li')
+list.append(main1)
+main1.innerText = input.value
+
 });
