3336b58c42d47784bf8c434f8ad38b2bbeca7068
zinakalu <zina.kalu@gmail.com>
2023-02-23 15:50:44 -0500

    tasklister project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const main = document.getElementById("main-content")
 const input = document.getElementById('new-task-description')
 const submitbtn = document.getElementById('submitbtn')
 const list = document.getElementById('list')
+// const deletebtn = document.getElementById('deletebtn')
 
 
 main.addEventListener("submit", (e) => {
@@ -12,5 +13,13 @@ e.preventDefault()
 const main1 = document.createElement('li')
 list.append(main1)
 main1.innerText = input.value
+const deletebtn1 = document.createElement('button')
+main1.append(deletebtn1)
+deletebtn1.innerText = "x"
+deletebtn1.addEventListener("click", (e)=>{
+  e.preventDefault()
+  main1.remove(deletebtn1)
 
+})
 });
+
